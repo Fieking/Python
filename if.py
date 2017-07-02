732b7e0ab898f1ccd5912280b1895431d7768a97
WIU\Jianfoo <cainefox@gmail.com>
2017-07-02 22:30:50 +0800

    update

--- if.py
+++ if.py
@@ -7,14 +7,11 @@ number = 23
 guess = int(raw_input('enter an intege:'))
 
 if guess = number:
-	print 'Congratulations,you guessed it.'  # New block starts here
-	print "(but you do not win any prizes!)" # New block ends here
+	print 'Congratulations,you guessed it.'  #New block starts here
+	print "(but you do not win any prizes!)" #New block ends here
 elif guess < number
-    print 'no,it is a little higher than that' # Another block
-    # you can do whatveer you want in a block ...
-else:
-	print 'no,it is a little lower than that'
+	print 'no,it is a little higher than that' # Another block
 	# you must have guess > number to reach heer
 
 print 'Done'
-# This last statement is always executed, after the if statement is executed
+	# This last statement is always executed, after the if statement is executed
